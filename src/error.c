a1b6f74736d165a94af8db5aedbe5c775462918b
Jay Takahashi <qsakura14555@gmail.com>
2024-06-05 13:48:18 +0900

    add color

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: jtakahas <jtakahas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 17:49:26 by jtakahas          #+#    #+#             */
-/*   Updated: 2024/06/04 17:49:27 by jtakahas         ###   ########.fr       */
+/*   Updated: 2024/06/05 13:44:51 by jtakahas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,10 @@
 
 void	error_and_exit(char *error_message, char *message, t_game *game)
 {
-	ft_printf("Error: %s", error_message);
+	ft_printf(RED"Error: %s"ENDC, error_message);
 	if (message)
-		ft_printf(" (%s)", message);
+		ft_printf("  (%s)", message);
 	ft_printf("\n");
 	if (game && game->mlx && game->window.win_ptr)
-	{
 		close_window(game);
-	}
-	exit(0);
 }
