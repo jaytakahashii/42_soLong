83c510b43e4d4de960d631c641203d6d10d9a507
Jay Takahashi <qsakura14555@gmail.com>
2024-06-09 11:55:15 +0900

    add comment

--- include/so_long.h
+++ include/so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jtakahas <jtakahas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jay <jay@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 18:11:07 by jtakahas          #+#    #+#             */
-/*   Updated: 2024/06/08 17:47:25 by jtakahas         ###   ########.fr       */
+/*   Updated: 2024/06/09 11:53:16 by jay              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@
 
 # include <stdbool.h>
 # include <fcntl.h>
-# include <stdio.h>
 
 # ifdef __APPLE__
+// key code for mac
 #  define ESC 53
 #  define UP 13
 #  define DOWN 1
@@ -33,21 +33,27 @@
 #  define DOWN_ARROW 125
 #  define LEFT_ARROW 123
 #  define RIGHT_ARROW 124
+// max size for mac window
 #  define MAX_WIDTH 2560
 #  define MAX_HEIGHT 1400
+// max size for mac image (42x42) : 2560 / 42 = 60, 1400 / 42 = 33
 #  define MAX_WIDTH_IMAGE 60
 #  define MAX_HEIGHT_IMAGE 33
+// max size for mac image (42x42) : 60 * 33 = 1980
 #  define MAX_IMAGE 1980
-#  define MAX_READ_SIZE 2970
-#  define READ_SIZE 3000
+// max size for mac read : (MAX_WIDTH_IMAGE + 1) * MAX_HEIGHT_IMAGE = 61 * 33 = 2013
+#  define MAX_READ_SIZE 2013
+#  define READ_SIZE 2500
 #  define DESTROYNOTIFY 17
+// 1L << 17
 #  define STRUCTURENOTIFYMASK 131072
 # endif
 
 # ifdef __linux__
-#  include <stdio.h>
+// include for linux miniLibX
 #  include <X11/X.h>
 #  include <X11/keysym.h>
+// key code for linux
 #  define ESC 65307
 #  define UP 119
 #  define DOWN 115
@@ -57,13 +63,17 @@
 #  define DOWN_ARROW 65364
 #  define LEFT_ARROW 65361
 #  define RIGHT_ARROW 65363
+// max size for linux window
 #  define MAX_WIDTH 1920
 #  define MAX_HEIGHT 1080
+// max size for linux image (42x42) : 1920 / 42 = 45, 1080 / 42 = 25
 #  define MAX_WIDTH_IMAGE 45
 #  define MAX_HEIGHT_IMAGE 25
+// max size for linux image (42x42) : 45 * 25 = 1125
 #  define MAX_IMAGE 1125
-#  define MAX_READ_SIZE 1688
-#  define READ_SIZE 2000
+// max size for linux read : (MAX_WIDTH_IMAGE + 1) * MAX_HEIGHT_IMAGE = 46 * 25 = 1150
+#  define MAX_READ_SIZE 1150
+#  define READ_SIZE 1500
 #  define DESTROYNOTIFY DestroyNotify
 #  define STRUCTURENOTIFYMASK StructureNotifyMask
 # endif
